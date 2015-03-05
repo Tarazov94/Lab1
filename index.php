26547ea6e0f835352dfe626cd90608580209b748
Tarazov94 <tarazov94@gmail.com>
2015-03-05 09:06:33 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Лаб1</title>
+<meta charset="utf8">
+ <link rel="stylesheet" type="text/css" href="styles/123.css">
 
+</head>
+
+<body>
+    <header>
+        <h1>Лаба 1</h1>
+    </header>
+    <nav>
+        <h1>Меню</h1>
+        <ul>
+            <li> <a href="http://vk.com">Вконтакте</a></li>
+            <li> <a href="http://yandex.ru">Яндекс</a></li>
+        </ul>
+    </nav>
+    <aside></aside>
+    <section>
+        <h1>Первый видео блок</h1>
+         <article>
+            <h2>Видео 1</h2>
+            <iframe width="560" height="315" src="http://www.youtube.com/embed/09R8_2nJtjg" frameborder="0" allowfullscreen></iframe>
+         </article>
+         <article>
+            <h2>Видео 2</h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/OZ_BIp8Yvsk" frameborder="0" allowfullscreen></iframe>
+         </article>
+         
+    </section>
+    
+    <section>
+        <h1>Второй видео блок</h1>
+         <article>
+            <h2>Видео 3</h2>
+            <iframe width="560" height="315" src="http://www.youtube.com/embed/RzFmWjavzYM" frameborder="0" allowfullscreen></iframe>
+         </article>
+         <article>
+            <h2>Видео 4</h2>
+            <iframe width="560" height="315" src="http://www.youtube.com/embed/89X2y9nGGzY" frameborder="0" allowfullscreen></iframe>
+         </article>
+         
+    </section>
+
+
+
+<footer>
+        <h1>Футер</h1>
+</footer>
+</body>
+
+
+
+</html>
